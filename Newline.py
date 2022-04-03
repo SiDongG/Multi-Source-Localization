58051b61c8fe10694486520e85b78c8a9a1faa6e
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-04-02 23:23:41 -0400

    Add files via upload

---
+++ Newline.py
@@ -0,0 +1,19 @@
+# Threshold Decision
+
+from Test import Estimation
+
+
+a=max(mic1)/10
+a1=max(mic2)/10
+a2=max(mic3)/10
+a3=max(mic4)/10
+a4=max(mic5)/10
+a5=max(mic6)/10
+a6=max(mic7)/10
+a7=max(mic8)/10
+avg_peak=(a+a1+a2+a3+a4+a5+a6+a7)/8
+
+if avg_peak>200
+
+if 24<Estimation<25:
+    Estimation==4
